832901dc7ca4fd763c040587b1255fa7bfb1db53
Tyler Biswell <tsbiswell@gmail.com>
2019-10-11 10:41:22 -0600

    added GET for projects endpoint

--- projects/project-router.js
+++ projects/project-router.js
@@ -8,8 +8,15 @@ const router = express.Router();
 
 // MVP Endpoints
 
-// GET /api/projects endpoint for Retrieving a list of projects -
-router.get('/', (req, res) => {});
+// GET /api/projects endpoint for Retrieving a list of projects - TESTED
+router.get('/', (req, res) => {
+    Projects.getProjects()
+      .then(projects => res.status(200).json(projects))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ message: 'Failed to get projects' });
+      });
+  });
 
 // POST /api/projects endpoint for Adding projects -
 router.post('/', (req, res) => {});
